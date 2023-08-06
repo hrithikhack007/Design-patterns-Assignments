9e63ed368ae3d7859b9a629a112231631f8cce64
hrithik.g <hrithik.g@internal.media.net>
2023-08-06 22:12:05 +0530

    adapter done

---
+++ designpattern/adapter/Adapter.java
@@ -0,0 +1,25 @@
+package net.media.training.designpattern.adapter;
+
+import net.media.training.designpattern.adapter.thirdparty.ThirdPartyLeaveRecord;
+
+public class Adapter implements LeaveRecord{
+
+    private ThirdPartyLeaveRecord thirdPartyLeaveRecord;
+
+    public Adapter(){
+        thirdPartyLeaveRecord = new ThirdPartyLeaveRecord();
+    }
+
+    public String getMostAbsentEmployee(){
+        Employee mostAbsentEmployee = thirdPartyLeaveRecord.getMostAbsentEmployee();
+        String name= mostAbsentEmployee.getName();
+        return name;
+    }
+
+    public int getEmployeeAbsences(String employeeName){
+        int employeeAbsences = thirdPartyLeaveRecord.getEmployeeAbsences(new Employee(employeeName));
+        return employeeAbsences;
+    }
+
+
+}
---
+++ designpattern/adapter/Main.java
@@ -0,0 +1,17 @@
+package net.media.training.designpattern.adapter;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Adapter adapter = new Adapter();
+
+        SalaryDisberser salaryDisberser = new SalaryDisberser(adapter);
+
+        System.out.println(salaryDisberser.salary("Bond, James"));
+
+        FrontDoorRegister frontDoorRegister = new FrontDoorRegister(adapter);
+
+        System.out.println(frontDoorRegister.getDisplayWallOfShame());
+    }
+}
